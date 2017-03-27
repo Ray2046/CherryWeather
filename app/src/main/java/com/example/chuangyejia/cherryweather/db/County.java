932ee6b37c40430deab12525f6d397676ca3d6c1
zhanglei <zl81946524@163.com>
2017-03-27 15:03:25 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/chuangyejia/cherryweather/db/County.java
+++ app/src/main/java/com/example/chuangyejia/cherryweather/db/County.java
@@ -9,7 +9,7 @@ import org.litepal.crud.DataSupport;
 public class County extends DataSupport {
 
     private int id;
-    private String coountyName;
+    private String countyName;
     private String weatherId;
     private int cityId;
 
@@ -21,12 +21,12 @@ public class County extends DataSupport {
         this.id = id;
     }
 
-    public String getCoountyName() {
-        return coountyName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCoountyName(String coountyName) {
-        this.coountyName = coountyName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
 
     public String getWeatherId() {
